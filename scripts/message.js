c28424ddab7126f8150e013ba9d7842591d73f30
Admin <admin@example.com>
2024-02-07 05:29:13 +0000

    HTML CSS Setup

---
+++ scripts/message.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", ()=>{
+    const messageContainer = document.querySelector("#message");
+    console.log(messageContainer);
+    messageContainer.innerHTML = "Hi World!!!";
+})
