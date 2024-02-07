cb4c9df1bf0676ebd5b8ba52f8309db7b69ba2fd
Admin <admin@example.com>
2024-02-07 06:34:23 +0000

    Object, arrays, and spread

--- scripts/message.js
+++ scripts/message.js
@@ -1,15 +1,31 @@
-// Map and Set
+const person = {
+  name: "abc",
+  age: 19,
+};
 
-const settings = new Map();
-settings.set("API_URL", "https://edureka.co/api/people");
-console.log(settings)
+const employment = {
+  title: "manager",
+  company: "Edureka",
+};
 
-const uniqueNumbers = new Set();
-uniqueNumbers.add(54)
-uniqueNumbers.add(54)
-uniqueNumbers.add(54)
-uniqueNumbers.add(65)
-uniqueNumbers.add(34)
-uniqueNumbers.add(34)
-uniqueNumbers.add(76)
-console.log(uniqueNumbers);
+const number = [2344,423,442,3423]
+const combined = {...person, ...employment, title : "Sr Manager", ...number}
+
+const personalContacts = [423421514353,55236254646]
+const corporateContacts = [4253546464,6456464,64563456546,64564563456]
+
+const contacts = [...personalContacts, ...corporateContacts, ...person];
+
+console.log(contacts)
+
+// combined.name = person.name;
+// combined.age = person.age;
+// combined.title = employment.title;
+// combined.company = employment.company;
+// Object.assign(combined, person, employment)
+
+
+
+
+
+console.log(combined);
