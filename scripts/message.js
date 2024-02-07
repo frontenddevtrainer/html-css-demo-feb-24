acd42ac5a3aca6b5a8f6c4dfb87a8ceb7bf8a6dd
Admin <admin@example.com>
2024-02-07 06:00:03 +0000

    Template Literal

--- scripts/message.js
+++ scripts/message.js
@@ -2,11 +2,12 @@ function abc(message) {
   return message + " - " + new Date().toISOString();
 }
 
-// one argument 
-const def = (message, message2) => message + "-" + message2 + " - " + new Date().toISOString();
-
+// one argument
+const def = (message, message2) =>
+  `${message} - ${message2} 
+  : ${new Date().toDateString()}
+  : ${new Date().toISOString()}`;
 
 console.log(abc("Hello World"));
 
 console.log(def("Hello World", "Hi World!!!"));
-
